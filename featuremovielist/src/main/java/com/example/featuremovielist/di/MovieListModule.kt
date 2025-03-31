17c5f57c5500f95a09d6b88c5bc2508a3195fa0a
luisj <luisjose_cg@hotmail.com>
2025-03-30 23:10:24 -0300

    added di

--- featuremovielist/src/main/java/com/example/featuremovielist/di/MovieListModule.kt
+++ featuremovielist/src/main/java/com/example/featuremovielist/di/MovieListModule.kt
@@ -1,10 +1,45 @@
 package com.example.featuremovielist.di
 
+import com.example.featuremovielist.data.remote.api.MovieListApiService
+import com.example.featuremovielist.data.repository.MovieRepositoryImpl
+import com.example.featuremovielist.domain.repository.MovieRepository
+import com.example.featuremovielist.domain.useCase.GetMoviesUseCase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object MovieListModule {
+
+    @Provides
+    fun provideBaseUrl() = "https://api.themoviedb.org/3/"
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(baseUrl: String): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideMovieApi(retrofit: Retrofit): MovieListApiService =
+        retrofit.create(MovieListApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideMovieRepository(api: MovieListApiService): MovieRepository =
+        MovieRepositoryImpl(api)
+
+    @Provides
+    @Singleton
+    fun provideGetMoviesUseCase(repository: MovieRepository): GetMoviesUseCase =
+        GetMoviesUseCase(repository)
+
 }
